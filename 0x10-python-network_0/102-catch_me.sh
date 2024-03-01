7b05c141441389e9ddf56882db92c90df0ab9966
ukemeedidiong <ukemeedidiong226@gmail.com>
2024-03-01 19:39:27 +0100

    second commit

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+# This script sends a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!".
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
