c5071853fbf745eb11c2386292670fe57db63d52
ukemeedidiong <ukemeedidiong226@gmail.com>
2024-03-01 19:56:10 +0100

    4th commit

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash scripts that sends a POST request to a given URL.
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# This script sends a POST request to a given URL with specific email and subject parameters. It uses curl in silent mode (-s) to display only the body of the response.
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
